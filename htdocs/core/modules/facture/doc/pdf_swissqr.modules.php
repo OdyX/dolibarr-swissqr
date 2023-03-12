8b87e9685cdbe57fd1bacca7fc0a58d953ac3c77
Didier 'OdyX' Raboud <odyx@raksha.ch>
2023-03-12 14:51:14 +0100

    cleanup: never add two SwissQRs, drop Dolibarr's

--- htdocs/core/modules/facture/doc/pdf_swissqr.modules.php
+++ htdocs/core/modules/facture/doc/pdf_swissqr.modules.php
@@ -460,7 +460,8 @@ class pdf_swissqr extends ModelePDFFactures
                 if (! empty($conf->global->INVOICE_ADD_ZATCA_QR_CODE)) {
                     $qrcodestring = $object->buildZATCAQRString();
                 } elseif (! empty($conf->global->INVOICE_ADD_SWISS_QR_CODE)) {
-                    $qrcodestring = $object->buildSwitzerlandQRString();
+                    // As we're about to display a SwissQR, don't add another one.
+                    // $qrcodestring = $object->buildSwitzerlandQRString();
                 }
                 if ($qrcodestring) {
                     $qrcodecolor = array('25', '25', '25');
